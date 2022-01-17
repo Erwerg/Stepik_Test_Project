29eb57c937d30ac83f7dfdff0f177c36e0cb5beb
Kirill Paley <tes3@nm.ru>
2022-01-17 14:13:01 -0800

    The code of adding to cart from product page (block 4.3)

--- pages/locators.py
+++ pages/locators.py
@@ -6,3 +6,10 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+class ProductPageLocators():
+    ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".col-sm-6.product_main > h1")
+    PRODUCT_NAME_IN_BASKET = (By.CSS_SELECTOR, ".alert-success > .alertinner > strong")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".col-sm-6.product_main > p.price_color")
+    PRICE_IN_BASKET = (By.CSS_SELECTOR, ".alert-info > .alertinner > p > strong")
